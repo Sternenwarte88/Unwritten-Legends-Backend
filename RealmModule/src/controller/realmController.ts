f293762bc3b11574d16c0c9fe8d5867392686f12
Sternenwarte88 <s.loecher.slc@gmail.com>
2025-02-19 23:12:46 +0100

    added getRealm, getAllRealms and assign realm

--- RealmModule/src/controller/realmController.ts
+++ RealmModule/src/controller/realmController.ts
@@ -30,3 +30,58 @@ export const createRealm = async (
     res.json({ isSuccessfull: false, message: 'Creating Realm failed', error: error });
   }
 };
+
+export const getAllRealms = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const realms: IRealmDocument[] = await Realms.find();
+
+    res.json({ isSuccessfull: true, data: realms });
+  } catch (error) {
+    console.log(error);
+    res.json({ isSuccessfull: false });
+  }
+};
+
+export const getRealm = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const id = req.body.id;
+
+  try {
+    const realm: IRealmDocument | null = await Realms.findOne({ realmId: id });
+
+    res.json({ isSuccessfull: true, data: realm });
+  } catch (error) {
+    console.log(error);
+    res.json({ isSuccessfull: false, message: 'Realm not found', error: error });
+  }
+};
+
+export const assignRealm = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  let realm: IRealmDocument | null;
+
+  try {
+    realm = await Realms.findOne().sort({ playerCount: 1 });
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
+    if (realm == null) {
+      throw new Error('smallest Realm is undefined');
+    }
+    realm.playerCount += 1;
+    realm.save();
+    res.json({ IsSuccessfull: true, data: realm });
+  } catch (error) {
+    console.error(error);
+    res.json({ IsSuccessfull: false, errorMessage: error });
+  }
+};
